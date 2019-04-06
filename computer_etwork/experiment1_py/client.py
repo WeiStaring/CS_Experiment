6aecec96d58970f18a4edd1e1128185124b90b63
WeiStaing <31434273+WeiStaing@users.noreply.github.com>
2019-04-06 20:30:02 +0800

    V1 finish

--- computer_etwork/experiment1_py/client.py
+++ computer_etwork/experiment1_py/client.py
@@ -1,18 +1,33 @@
-from socket import *
+import socket,select,threading
 
-def main():
-    # 创建套接
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    # 连接
-    clientSocket.connect(('127.0.0.1', 8899))
 
-    clientSocket.send('haha'.encode('utf-8'))
+class Client:
+    def __init__(self):
+        self.client_socket = socket.socket()
+        self.host = socket.gethostname()
+        self.client_addr = (self.host,8899)
+        self.client_socket.connect(self.client_addr)
+        listen = threading.Thread(target=self.listening)
+        listen.start()
+        while True:
+            try:
+                user_input = input(">>>")
+            except Exception as e:
+                continue
+            self.client_socket.send(user_input.encode('utf8'))
+            if user_input == 'quit':
+                break
 
-    recvData = clientSocket.recv(1024)
-    print('recvData:%s' %recvData)
+    def listening(self):
+        inputs = [self.client_socket]
+        while True:
+            rlist, wlist, elist = select.select(inputs, [], [])
+            if self.client_socket in rlist:
+                try:
+                    print(self.client_socket.recv(1024))
+                except socket.error:
+                    print("socket break down")
+                    exit()
 
-    # 关闭套接字
-    clientSocket.close()
-
-if __name__ == "__main__" :
-    main()
+if __name__ == "__main__":
+    client = Client()
--- computer_etwork/experiment1_py/server.py
+++ computer_etwork/experiment1_py/server.py
@@ -1,22 +1,66 @@
 from socket import *
+import select
+import threading
 
-# 创建套接字
-serverSocket = socket(AF_INET, SOCK_STREAM)
-# 绑定端口
-serverSocket.bind(("", 8899))
-# 监听事件
-serverSocket.listen(5)
-while True:
-    # 接收请求
-    clientSocket, clientInfo = serverSocket.accept()
-    while True:
-        recvData = clientSocket.recv(1024)
-        if len(recvData)>0:
-            print("%s:%s" %(str(clientInfo), recvData))
-        else:
-            break
 
-        sendData = input('send:')
-        clientSocket.send(bytes(sendData))
-    # 关闭套接字
-    clientSocket.close()
+class Server:
+    def __init__(self):
+        self.serverSocket = socket(AF_INET, SOCK_STREAM)
+        self.serverSocket.bind(("", 8899))
+        self.serverSocket.listen(5)
+        self.fd_name = {}  # nickname map
+        self.inputs = []   # listen
+        self.inputs.append(self.serverSocket)
+        print("server is running")
+
+    def new_connect(self):
+        clientSocket, clientInfo = self.serverSocket.accept()
+        try:
+            clientSocket.send(bytes("welcome to chatroom,pls set up your nick name!", encoding='utf8'))
+            client_name = clientSocket.recv(1024)
+            self.fd_name[clientSocket] = client_name
+            self.inputs.append(clientSocket)
+            print(client_name, 'enter the room')
+            self.forcast(self.fd_name[clientInfo] + bytes("join",encoding='utf8'), clientSocket)
+        except Exception as e:
+            print(e)
+
+    def run(self):
+        while True:
+            rlist, wlist, elist = select.select(self.inputs, [], [])
+            if not rlist:
+                print('out')
+                self.serverSocket.close()
+                break
+            for r in rlist:
+                if r is self.serverSocket:
+                    self.new_connect()
+                else:
+                    try:
+                        data = r.recv(1024)
+                        data = self.fd_name[r] + bytes(":",encoding='utf8') + data
+                    except error:
+                        data = self.fd_name[r] + bytes("leaved the room",encoding='utf8')
+                        print(self.fd_name[r], 'leave the room')
+                        self.inputs.remove(r)
+                        del self.fd_name[r]
+                    self.forcast(data, r)
+
+    def forcast(self, mess, excepts):
+        for socket in self.fd_name.keys():
+            if socket != excepts and socket != self.serverSocket:
+                socket.send(mess)
+
+    def remove(self, r, data):
+        self.inputs.remove(r)
+        print(data)
+        self.forcast(data, r)
+
+
+def main():
+    server = Server()
+    server.run()
+
+
+if __name__ == "__main__":
+    main()
